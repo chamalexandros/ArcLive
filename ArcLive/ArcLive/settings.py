f09941a2160a8e8b11f77f88cbda42b5227e88fb
chamalexandros <chamalexandros@gmail.com>
2026-01-18 12:41:48 +0900

    settings.pyにログアウト後にログイン画面を表示するように設定

--- ArcLive/ArcLive/settings.py
+++ ArcLive/ArcLive/settings.py
@@ -57,7 +57,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'records'
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -159,3 +160,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDIRECT_URL = "" #ログイン後に表示するページを指定 例："/diary/"
+LOGOUT_REDIRECT_URL = "/login/"
