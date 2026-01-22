c2a465fda996ce3e623bfb538e8d35e0f59232c4
chamalexandros <chamalexandros@gmail.com>
2026-01-22 14:28:24 +0900

    accounts/views.pyにIndexViewクラス追加

--- ArcLive/accounts/views.py
+++ ArcLive/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'accounts/index.html')
+
+index =IndexView.as_view()
 
 # Create your views here.
