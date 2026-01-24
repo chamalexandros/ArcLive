a5f620ae85c62e9886b26ef6ab9464d42806b396
chamalexandros <chamalexandros@gmail.com>
2026-01-24 21:39:07 +0900

    records/models.pyにRecord, Artist_Record, Artist, Venue classを作成

--- ArcLive/records/models.py
+++ ArcLive/records/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    user_id =
+    venue_id =
+    event_name = models.CharField(max_length=100, blank=True, null=True)
+    live_date = models.DateField() #公演日(必須)
+    live_image = models.TextField(blank=True, null=True) #参戦記録一覧画面に表示する画像
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+class Artist_Record(models.Model):
+    record_id =
+    artist_id =
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100) #アーティスト名
+    reading_name = models.CharField(max_length=100) #よみがな
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+class Venue(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True) #会場名
+    location = models.CharField(max_length=100, blank=True, null=True) #会場所在地
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+class Design_Setting(models.Model):
+    preset_image_id = 
+    user_image_id = 
+    record_id =
+    font_type = models.CharField(max_length=100)
+    font_color = models.CharField(max_length=100)
+    start_date = models.DateField() #表示する参戦記録の開始日
+    end_date = models.DateField() #表示する参戦記録の終了日
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
