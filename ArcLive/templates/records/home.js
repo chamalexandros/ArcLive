cda2cfd595e63f9bc54facec7d59404bf5f5cf48
chamalexandros <chamalexandros@gmail.com>
2026-01-24 09:38:00 +0900

    templates/records内にhome.jsファイルを作成し、日付選択の固定を現在日に設定

---
+++ ArcLive/templates/records/home.js
@@ -0,0 +1,6 @@
+$(function(){
+    var year = new Date().getFullYear();
+    var month = new Date().getMonth();+1
+    var day = new Date().getDate();
+    $("date1").html(year + "年" + month + "月" + date + "日" 
+});
