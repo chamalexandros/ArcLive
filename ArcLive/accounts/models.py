2f0a88bf8e777a6e49d87d79b3e76e81a90fd0f7
chamalexandros <chamalexandros@gmail.com>
2026-01-24 21:40:34 +0900

    accounts/models.pyにUser, User_Image, Preset_Image classを作成

--- ArcLive/accounts/models.py
+++ ArcLive/accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Preset_Image(models.Model):
+    preset_image_url = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+class User_Image(models.Model):
+    user_id = 
+    user_image_url = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+class User(models.Model):
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    last_login_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
