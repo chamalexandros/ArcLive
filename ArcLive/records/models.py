c2c2609916db408c0da5e5e221d54e23d2eb313b
chamalexandros <chamalexandros@gmail.com>
2026-01-25 15:33:35 +0900

    records/models.pyのFKwo

--- ArcLive/records/models.py
+++ ArcLive/records/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Record(models.Model):
-    user_id =
-    venue_id =
+    user_id = models.ForeignKey('accounts.User',on_delete=models.CASCADE)
+    venue_id = models.ForeignKey('Venue',on_delete=models.CASCADE)
     event_name = models.CharField(max_length=100, blank=True, null=True)
     live_date = models.DateField() #公演日(必須)
     live_image = models.TextField(blank=True, null=True) #参戦記録一覧画面に表示する画像
@@ -11,8 +11,8 @@ class Record(models.Model):
 
 
 class Artist_Record(models.Model):
-    record_id =
-    artist_id =
+    record_id = models.ForeignKey('Record', on_delete=models.CASCADE)
+    artist_id = models.ForeignKey('Artist', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
@@ -32,9 +32,9 @@ class Venue(models.Model):
 
 
 class Design_Setting(models.Model):
-    preset_image_id = 
-    user_image_id = 
-    record_id =
+    preset_image_id = models.ForeignKey('accounts.Preset_Image',on_delete=models.CASCADE)
+    user_image_id = models.ForeignKey('accounts.User_Image',on_delete=models.CASCADE)
+    record_id = models.ForeignKey('Record', on_delete=models.CASCADE)
     font_type = models.CharField(max_length=100)
     font_color = models.CharField(max_length=100)
     start_date = models.DateField() #表示する参戦記録の開始日
