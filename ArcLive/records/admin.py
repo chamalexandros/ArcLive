2ce8adaab4c5ac37962fc56924549e43b2127447
chamalexandros <chamalexandros@gmail.com>
2026-01-26 21:17:02 +0900

    accounts/admin.pyに管理画面に表示させたい項目(Record,Artist_Record,Artist,Venue,Design_Setting)を追加

--- ArcLive/records/admin.py
+++ ArcLive/records/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Record, Artist_Record, Artist, Design_Setting, Venue
 
-# Register your models here.
+admin.site.register(Record)
+admin.site.register(Artist_Record)
+admin.site.register(Artist)
+admin.site.register(Design_Setting)
+admin.site.register(Venue)
