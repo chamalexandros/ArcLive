b3c04de008d63816174ecc300082d563582d1535
chamalexandros <chamalexandros@gmail.com>
2026-01-26 21:15:35 +0900

    accounts/admin.pyに管理画面に表示させたい項目(User,User_Image,Preset_Image)を追加

--- ArcLive/accounts/admin.py
+++ ArcLive/accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, User_Image, Preset_Image
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(User_Image)
+admin.site.register(Preset_Image)
