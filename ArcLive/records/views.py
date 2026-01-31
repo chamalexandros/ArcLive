ef6d510d24a294bd7ea1436f445df33e3fbc5b17
chamalexandros <chamalexandros@gmail.com>
2026-01-31 11:25:47 +0900

    records/views.pyにrecord = form.save(commit=False) record.user_id = request.userを追加

--- ArcLive/records/views.py
+++ ArcLive/records/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from .forms import RecordForm
 
 class HomeView(View):
     def get(self, request):
         return render(request, 'records/home.html')
 
-home =HomeView.as_view()
 
+class RecordCreateView(View):
+    def get(self, request):
+        form = RecordForm()
+        return render(request, "records/recordcreate_form.html", {"form":form})
+    
+    def post(self, request):
+        form = RecordForm(request.POST)
+        if form.is_valid():
+            record = form.save(commit=False)
+            record.user_id = request.user
+            form.save()
+            return redirect("records:home")
+        return render(request, "records/recordcreate_form.html", {"form": form})
+
+home =HomeView.as_view()
+recordcreate = RecordCreateView.as_view()
     
 
 
