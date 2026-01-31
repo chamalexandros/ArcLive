8aa84d8ec2e79a7521e6918708ae42eaddb83c5d
chamalexandros <chamalexandros@gmail.com>
2026-01-31 15:30:21 +0900

    urlpatternsのhomeページのパスを追加

--- ArcLive/records/urls.py
+++ ArcLive/records/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'records'
 urlpatterns = [
-        path("", views.home, name='home'), #1つ目のURLを通ったらviews.homeを動かすようにする。このURLの名前はhomeという意味
+        path("record/home", views.home, name='home'), #1つ目のURLを通ったらviews.homeを動かすようにする。このURLの名前はhomeという意味
         path("record/create/", views.recordcreate, name='recordcreate'),
         
     ]
