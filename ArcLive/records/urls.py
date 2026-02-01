86b6e61b3908986c0e1c081d0338dc9ea016e36e
chamalexandros <chamalexandros@gmail.com>
2026-02-01 10:57:06 +0900

    records/urls.pyにrecord/list/のパス追加

--- ArcLive/records/urls.py
+++ ArcLive/records/urls.py
@@ -6,7 +6,7 @@ app_name = 'records'
 urlpatterns = [
         path("record/home", views.home, name='home'), #1つ目のURLを通ったらviews.homeを動かすようにする。このURLの名前はhomeという意味
         path("record/create/", views.recordcreate, name='recordcreate'),
-        path("record/create/", views.recordlist, name='recordcreate'),
+        path("record/list/", views.recordlist, name='recordcreate'),
         
     ]
     
