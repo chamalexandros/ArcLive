b4638fd5132ef2fd258ee0ce5ecd8e2fa30431e7
chamalexandros <chamalexandros@gmail.com>
2026-02-09 20:30:54 +0900

    accounts/urls.pyにlogin,logout,mypage,change_email,change_password,signup,password_reset,password_reset/done,reset/<uidb64>/<token>,reset/doneのパスを追加

--- ArcLive/accounts/urls.py
+++ ArcLive/accounts/urls.py
@@ -1,9 +1,24 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.urls import path, reverse_lazy
+from . import views
+from django.contrib.auth import views as auth_views
 
 
 app_name = "accounts"
+
 urlpatterns = [
     path('login/', LoginView.as_view(), name= 'login'),
     path('logout/', LogoutView.as_view(), name= 'logout'),
+    path('mypage/', views.mypage, name="mypage"),
+    path('change_email/', views.change_email, name="change_email"),
+    path('change_password/', views.change_password, name="change_password"),
+    path('signup/', views.signup, name="signup"),
+    # パスワード再発行フォーム
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'), 
+    # パスワード再発行メール送信完了
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # パスワード再設定
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # パスワード再設定完了
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
