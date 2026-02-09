ba6fa0be3a3899a45e064894c817497880e6e634
chamalexandros <chamalexandros@gmail.com>
2026-02-09 20:13:21 +0900

    records/forms.pyに(RecordForm,ArtistForm,VenueForm,RecordMultiForm,SearchForm)を追加

--- ArcLive/records/forms.py
+++ ArcLive/records/forms.py
@@ -1,7 +1,58 @@
-from django.forms import ModelForm
-from .models import Record
+from django.forms import ModelForm, CharField, ImageField
+from .models import Record, Artist, Venue
+from django import forms
+from betterforms.multiform import MultiModelForm
 
+#公演日・イベント名・参戦時の画像登録
 class RecordForm(ModelForm):
+    
+    #artist_name = CharField(max_length=100, label="アーティスト名")
+    #venue_name = CharField(max_length=100, required=False, label="会場名")
+    
     class Meta:
         model = Record
-        fields = ["live_date", "event_name", ]
+        fields = ["live_date", "event_name", "live_image"]
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+
+class ArtistForm(ModelForm):
+    class Meta:
+        model = Artist
+        fields = ["name"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class VenueForm(ModelForm):
+    class Meta:
+        model = Venue
+        fields = ["name"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        
+    
+class RecordMultiForm(MultiModelForm):
+    form_classes = {
+        "record_form": RecordForm,
+        "artist_form": ArtistForm,
+        "venue_form": VenueForm
+    }
+#self.order_fields(['A','B'])にするとその順に並び替えてくれる
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        #self.field_names(["record_form/live_date", "artist_form/name","record_form/event_name","venue_form/name","record_form/live_image"])
+
+
+
+
+class SearchForm(forms.Form):
+
+    aritst = forms.CharField(
+        initial='',
+        label='アーティスト名などのキーワード',
+        required = False, # 必須ではない
+    )
