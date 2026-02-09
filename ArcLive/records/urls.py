2aafd01a91d69b68194d4adcb0ce959258ce52d8
chamalexandros <chamalexandros@gmail.com>
2026-02-09 20:20:39 +0900

    records/urls.pyに(ホーム画面、参戦記録新規登録画面、参戦記録一覧画面、参戦記録更新、参戦記録削除)のパスを追加

--- ArcLive/records/urls.py
+++ ArcLive/records/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
         path("record/home", views.home, name='home'), #1つ目のURLを通ったらviews.homeを動かすようにする。このURLの名前はhomeという意味
         path("record/create/", views.recordcreate, name='recordcreate'),
         path("record/list/", views.recordlist, name='recordlist'),
+        path("record/<int:id>/", views.recordedit, name="recordedit"),
+        path("record/<int:id>/update/", views.recordupdate, name="recordupdate"),
+        path("record/<int:id>/delete/", views.recorddelete, name="recorddelete"),
         
     ]
     
