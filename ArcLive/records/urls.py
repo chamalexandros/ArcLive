5ed0e459fd2e50281d82bf0d66ffc0e6fc73edf5
chamalexandros <chamalexandros@gmail.com>
2026-02-15 12:58:29 +0900

    nameの表記を統一

--- ArcLive/records/urls.py
+++ ArcLive/records/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'records'
 urlpatterns = [
         path("record/home", views.home, name='home'), #1つ目のURLを通ったらviews.homeを動かすようにする。このURLの名前はhomeという意味
-        path("record/create/", views.recordcreate, name='recordcreate'),
-        path("record/list/", views.recordlist, name='recordlist'),
-        path("record/<int:id>/", views.recordedit, name="recordedit"),
-        path("record/<int:id>/update/", views.recordupdate, name="recordupdate"),
-        path("record/<int:id>/delete/", views.recorddelete, name="recorddelete"),
+        path("record/create/", views.record_create, name='record_create'),
+        path("record/list/", views.record_list, name='record_list'),
+        path("record/<uuid:pk>/", views.record_edit, name="record_edit"),
+        path("record/<uuid:pk>/update/", views.record_update, name="record_update"),
+        path("record/<uuid:pk>/delete/", views.record_delete, name="record_delete"),
         
     ]
     
