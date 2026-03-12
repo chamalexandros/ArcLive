08d9e2959caad48c28c0f870ea2099c3516f15f4
chamalexandros <chamalexandros@gmail.com>
2026-03-12 21:16:01 +0900

    ArcLive/settings.pyにアプリ専用のgmailを追加

--- ArcLive/ArcLive/settings.py
+++ ArcLive/ArcLive/settings.py
@@ -161,26 +161,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 LOGIN_REDIRECT_URL = "records:home" #ログイン後に表示するページを指定 例："/diary/"
 LOGOUT_REDIRECT_URL = "/login/"
 
 AUTH_USER_MODEL = 'accounts.User'
 
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR / "media")
 
 MEDIA_URL = "/media/"
 
 
     # 本番環境ではSMTPサーバーを使ってメール送信
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "chamalexandros@gmail.com"
-EMAIL_HOST_PASSWORD = "nvme jlzu afab xmql"
+EMAIL_HOST_USER = "ArcLive70@gmail.com"
+EMAIL_HOST_PASSWORD = "fntf xzhe hezd lybn"
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = "ArcLive<chamalexandros@gmail.com>"
+DEFAULT_FROM_EMAIL = "ArcLive70@gmail.com"
     
 
-AUTHENTICATION_BACKEND = ['django.contrib.auth.backends.ModelBackend']
+#AUTHENTICATION_BACKEND = ['django.contrib.auth.backends.ModelBackend']
