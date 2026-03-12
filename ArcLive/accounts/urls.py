d763fb98a110704072655211e13611748a42de3a
chamalexandros <chamalexandros@gmail.com>
2026-03-12 21:18:12 +0900

    accounts/urls.pyにパスワード再設定メール送信に必要なパスを追加

--- ArcLive/accounts/urls.py
+++ ArcLive/accounts/urls.py
@@ -13,12 +13,8 @@ urlpatterns = [
     path('change_email/', views.change_email, name="change_email"),
     path('change_password/', views.change_password, name="change_password"),
     path('signup/', views.signup, name="signup"),
-    # パスワード再発行フォーム
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'), 
-    # パスワード再発行メール送信完了
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    # パスワード再設定
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    # パスワード再設定完了
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset/', views.password_reset, name='password_reset'), 
+    path('password_reset_done/', views.password_reset_done, name='password_reset_done'), 
+    path('password_reset_confirm/<uidb64>/<token>/', views.password_reset_confirm, name='password_reset_confirm'), 
+    path('password_reset_complete/', views.password_reset_complete, name='password_reset_complete'), 
 ]
