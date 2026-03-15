542a7f058b43b0e5bc13d6ba8cf6fb0b584cbcb2
chamalexandros <chamalexandros@gmail.com>
2026-03-15 12:26:23 +0900

    records/urls.pyを変更した

--- ArcLive/records/urls.py
+++ ArcLive/records/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'records'
 urlpatterns = [
-        path("record/home", views.home, name='home'), #1つ目のURLを通ったらviews.homeを動かすようにする。このURLの名前はhomeという意味
+        path("record/home", views.home, name='home'), #1つ目のURLを通ったらviews.homeを動かすようにする。このURLの名前はhomeという意味     
         path("record/create/", views.record_create, name='record_create'),
         path("record/list/", views.record_list, name='record_list'),
         path("record/<uuid:pk>/", views.record_edit, name="record_edit"),
